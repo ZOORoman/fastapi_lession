26b9e93284d44db44064716ea2f13ffa5644adce
zooroman <prof-roman@bk.ru>
2023-04-30 19:29:19 +0300

    Update prj Fourth

---
+++ lession/fourth/src/operations/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP
+
+
+metadata = MetaData()
+
+operation = Table(
+    "operation",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("quantity", String),
+    Column("figi", String),
+    Column("instrument_type", String, nullable=True),
+    Column("date", TIMESTAMP, default=datetime.utcnow),
+    Column("type", String),
+)
